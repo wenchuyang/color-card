39e31ee41e939052d05af821daa26ef76d368b34
wenchuyang <wenchuyang@foxmail.com>
2022-12-14 20:25:20 +0800

    环境配置

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Home from './pages/home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+function App () {
+    return (
+        <div className="App">
+            <Home />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+/*
+ * FileName:
+ * Created Date: 2022-12-14 13:15:10
+ * Author: 文初阳
+ * -----
+ * Last Modified: 2022-12-14 17:55:24
+ * Modified By:
+ */
+
+import * as React from "react";
+// import style from './Home.module.scss';
+
+const Home: React.FC = () => {
+    return <div>222</div>;
+};
+export default Home;

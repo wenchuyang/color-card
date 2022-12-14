9496e6692c049677f64c0c7fc8f7ccfc34195c81
wenchuyang <wenchuyang@foxmail.com>
2022-12-14 23:07:05 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import Home from './pages/home/Home';
+import "./App.css";
+import Home from "./pages/home/Home.tsx";
 
 function App () {
     return (
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,14 +3,14 @@
  * Created Date: 2022-12-14 13:15:10
  * Author: 文初阳
  * -----
- * Last Modified: 2022-12-14 17:55:24
+ * Last Modified: 2022-12-14 22:54:24
  * Modified By:
  */
 
-import * as React from "react";
-// import style from './Home.module.scss';
+import React from "react";
+import style from "./Home.module.scss";
 
 const Home: React.FC = () => {
-    return <div>222</div>;
+    return <div className={style.red}>222</div>;
 };
 export default Home;

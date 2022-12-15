bb95c4261ba564fe9cf1f792e387f5e1534e4c4d
wenchuyang <wenchuyang@foxmail.com>
2022-12-15 22:19:47 +0800

    添加 title 样式，修改字体

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,7 @@
  * Created Date: 2022-12-14 13:15:10
  * Author: 文初阳
  * -----
- * Last Modified: 2022-12-15 14:19:53
+ * Last Modified: 2022-12-15 18:09:06
  * Modified By: 文初阳
  */
 
@@ -12,8 +12,10 @@ import style from "./Home.module.scss";
 import Title from "./title/Title";
 
 const Home: React.FC = () => {
-    return <div className={style.red}>
-        <Title />
-    </div>;
+    return (
+        <div className={style.wrapper}>
+            <div className={style.title}><Title /></div>
+        </div>
+    );
 };
 export default Home;
--- src/pages/home/title/Title.tsx
+++ src/pages/home/title/Title.tsx
@@ -3,17 +3,17 @@
  * Created Date: 2022-12-15 14:03:51
  * Author: 文初阳
  * -----
- * Last Modified: 2022-12-15 14:13:33
+ * Last Modified: 2022-12-15 22:12:28
  * Modified By: 文初阳
  */
 import React from "react";
 import style from "./Title.module.scss";
-import logo from "../../../assets/logo.svg";
+import logo from "../../../assets/imgs/logo.svg";
 
 const Title: React.FC = () => {
     return <header className={style.wrapper}>
-        <img src={logo} alt="logo" />
-        <h1>中国传统色</h1>
+        <img src={logo} alt="logo" width="58px" />
+        <h1 className={style.title}>中国传统色</h1>
     </header>;
 };
 export default Title;

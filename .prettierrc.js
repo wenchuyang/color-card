0f20d383e80d47657725961ec6650474b39c8119
wenchuyang <writing_happy@163.com>
2023-02-03 15:27:41 +0800

    添加格式化规则

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/**
+ * FileName: prettier 配置文件
+ * Created Date: 2023-02-03 14:13:28
+ * Author: 文初阳
+ * -----
+ * Last Modified: 2023-02-03 14:18:06
+ * Modified By: 文初阳
+ */
+module.exports = {
+    "semi": true,
+    "tabWidth": 4,
+    "useTabs": false,
+    "singleQuote": false,
+    "trailingComma": "es6",
+    "printWidth": 120,
+    "bracketSpacing": true
+}

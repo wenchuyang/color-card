0ae67319ee3da2326360ae403527ae4a16b01b16
wenchuyang <writing_happy@163.com>
2023-02-04 22:27:49 +0800

    修改格式化配置文件

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,15 +3,19 @@
  * Created Date: 2023-02-03 14:13:28
  * Author: 文初阳
  * -----
- * Last Modified: 2023-02-03 14:18:06
+ * Last Modified: 2023-02-04 22:17:31
  * Modified By: 文初阳
  */
 module.exports = {
-    "semi": true,
-    "tabWidth": 4,
-    "useTabs": false,
-    "singleQuote": false,
-    "trailingComma": "es6",
-    "printWidth": 120,
-    "bracketSpacing": true
-}
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: false,
+    trailingComma: "none",
+    printWidth: 100,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    jsxBracketSameLine: false,
+    proseWrap: "always",
+    arrowParens: "avoid"
+};
